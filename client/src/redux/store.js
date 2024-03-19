ed24dcf38b1b16a49156cb9ac101af9d873cccef
Nikhilmk27 <nikhilchandranmk9526@gmail.com>
2024-03-20 00:11:30 +0530

    impliented redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'; // Correct import statement
+import { combineReducers, configureStore } from '@reduxjs/toolkit'; // Correct import statement
 import userReducer from './user/userSlice';
+import {persistReducer,persistStore} from 'redux-persist'
+import storage  from 'redux-persist/lib/storage';
 
+
+const rootReducer = combineReducers({user:userReducer})
+const persistconfig = {
+  key: 'root',
+  version:1,
+  storage,
+}
+const persistedReducer = persistReducer(persistconfig , rootReducer)
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer:persistedReducer ,
   middleware: (getdefaultMiddleware) =>
     getdefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store)
